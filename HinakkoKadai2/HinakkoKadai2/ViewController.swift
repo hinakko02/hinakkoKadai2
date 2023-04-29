96b0267703365988c42939ecf50a6ae985d7af8b
akio0911 <akio0911@gmail.com>
2023-04-29 13:37:15 +0900

    feedback

--- HinakkoKadai2/HinakkoKadai2/ViewController.swift
+++ HinakkoKadai2/HinakkoKadai2/ViewController.swift
@@ -13,23 +13,25 @@ class ViewController: UIViewController {
     @IBOutlet weak var calculatedControl: UISegmentedControl!
     @IBOutlet weak var label: UILabel!
 
-    @IBAction func PushButton(_ sender: Any) {
-        let textField1 = Float(textField1.text ?? "") ?? 0
-        let textField2 = Float(textField2.text ?? "") ?? 0
+    @IBAction func pushButton(_ sender: Any) {
+        let value1 = Float(textField1.text ?? "") ?? 0
+        let value2 = Float(textField2.text ?? "") ?? 0
 
-        if calculatedControl.selectedSegmentIndex == 0 {
-            label.text = String(textField1 + textField2) //なんでFloat型使えないの
-        }else if calculatedControl.selectedSegmentIndex == 1 {
-            label.text = String(textField1 - textField2)
-        }else if calculatedControl.selectedSegmentIndex == 2 {
-            label.text = String(textField1 * textField2)
-        }else if calculatedControl.selectedSegmentIndex == 3 {
-            label.text = String(textField1 / textField2)
-            if textField1 == 0||textField2 == 0 {
+        switch calculatedControl.selectedSegmentIndex {
+        case 0:
+            label.text = String(value1 + value2) //なんでFloat型使えないの
+        case 1:
+            label.text = String(value1 - value2)
+        case 2:
+            label.text = String(value1 * value2)
+        case 3:
+            if value2 == 0 {
                 label.text = "割る数には0以外を入力してください"
+            } else {
+                label.text = String(value1 / value2)
             }
+        default:
+            assertionFailure("selectedSegmentIndex is invalid.")
         }
     }
 }
-
-
